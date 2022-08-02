f01f878b4cf96eea9405765745df596fee56e55b
vagrant <vagrant@vagrant.vm>
2022-08-02 16:58:50 +0900

    make post index view and configure route action

--- routes/front.php
+++ routes/front.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\PostController;
 
-Route::get('/', function () {
-    echo 'front';
-});
+Route::get('/', 'PostController@index')->name('home');
+Route::resource('posts', 'PostController')->only(['index','show']);
