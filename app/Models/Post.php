7722ded1e0a8115f3bc61a9d684399d2862eae9e
vagrant <vagrant@vagrant.vm>
2022-08-02 14:58:40 +0900

    configure routes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,7 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Post
+ *
+ * @method static \Database\Factories\PostFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post query()
+ * @mixin \Eloquent
+ */
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'body', 'is_public', 'published_at'
+    ];
+
+    protected $casts = [
+        'is_public' => 'bool',
+        'published_at' => 'datetime'
+    ];
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,20 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
-    /**
-     * The controller namespace for the application.
-     *
-     * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
-     */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->configureRateLimiting();
@@ -43,12 +31,20 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
+            // フロント画面
             Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
+                ->namespace($this->namespace . '\Front')
+                ->as('front.')
+                ->group(base_path('routes/front.php'));
+
+            // 管理画面
+            Route::prefix('admin')
+                ->middleware('web')
+                ->namespace($this->namespace . '\Back')
+                ->as('back.')
+                ->group(base_path('routes/back.php'));
         });
     }
-
     /**
      * Configure the rate limiters for the application.
      *
