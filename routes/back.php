8f5c23ca3bb4eccfa04a6430567d7e7547e829d9
vagrant <vagrant@vagrant.vm>
2022-08-03 15:12:56 +0900

    login page not found

--- routes/back.php
+++ routes/back.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    Route::get('/', 'DashboardController')->name('dashboard');
-});
+Route::get('/', 'DashboardController')->name('dashboard');
+Route::get('/login', [GoogleLoginController::class, 'getGoogleAuth']);
+Route::get('/login/callback', [GoogleLoginController::class, 'authGoogleCallback']);
